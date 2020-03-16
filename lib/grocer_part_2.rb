39089c40f36064c80a45944b39804b5266db343d
maxwellbenton <maxwellbenton@gmail.com>
2020-03-16 09:55:58 -0400

    set starter code, move file

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,15 +1,4 @@
-def find_item_by_name_in_collection(name, collection)
-  # Implement me first!
-  #
-  # Consult README for inputs and outputs
-end
-
-def consolidate_cart(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This returns a new Array that represents the cart. Don't merely
-  # change `cart` (i.e. mutate) it. It's easier to return a new thing.
-end
+require_relative './part_1_solution.rb'
 
 def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs

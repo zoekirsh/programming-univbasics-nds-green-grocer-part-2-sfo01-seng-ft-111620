feffa0e4eb19f9bc4ae7b4ba817fc00b64478f83
Zoe Kirsh <zoekirsh@gmail.com>
2020-11-14 02:59:58 +0000

    Done.

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,25 +1,42 @@
 require_relative './part_1_solution.rb'
+require 'pry'
 
 def apply_coupons(cart, coupons)
-  # Consult README for inputs and outputs
-  #
   # REMEMBER: This method **should** update cart
+  coupons.each do |couponHash|
+    currentItem = find_item_by_name_in_collection(couponHash[:item], cart)
+    if currentItem && currentItem[:count] >= couponHash[:num]
+      cart << {:item => currentItem[:item] + " W/COUPON", :price => couponHash[:cost] / couponHash[:num], :clearance => currentItem[:clearance], :count => couponHash[:num]}
+      currentItem[:count] -= couponHash[:num]
+    end
+  end
+  return cart
 end
 
 def apply_clearance(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  cart.each do |itemHash|
+    if itemHash[:clearance] == true
+      itemHash[:price] = (0.8 * itemHash[:price]).round(2)
+    end
+  end
+  return cart
 end
 
 def checkout(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # This method should call
-  # * consolidate_cart
-  # * apply_coupons
-  # * apply_clearance
-  #
-  # BEFORE it begins the work of calculating the total (or else you might have
-  # some irritated customers
+ # Call consolidate_cart
+ # Call apply_coupons && apply_clearance
+ # Iterate thru mutated cart and get a grand total with each hash's price * count
+ # If grand total over 100, apply 10% discount 
+ bigCart = apply_clearance(apply_coupons(consolidate_cart(cart), coupons))
+ grandTotal = 0
+ bigCart.each do |itemHash|
+   grandTotal += itemHash[:price] * itemHash[:count]
+ end
+ grandTotal >= 100? grandTotal * 0.9 : grandTotal
 end
+
+
+
+
+
+
